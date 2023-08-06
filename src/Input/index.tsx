410703bc7485610e4473de5dcc736b24a27c3836
阳洋 卢 <1972887669@qq.com>
2023-08-06 23:38:06 +0800

    first commit

---
+++ src/Input/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import './style.less'
+import {type sizeType} from '../utils/config'
+import { type FC } from 'react'
+
+type inputType='text'|'password'
+
+interface Iporps{
+    size?:sizeType,
+    value?:string,
+    type?:inputType,
+    placeholder?:string,
+    children?:React.ReactDOM[]|React.ReactDOM
+}
+
+const Input:FC<Iporps> = (props) => {
+
+    let {size,value,type,placeholder} = props
+    size = size?size:'medium'
+    type = type?type:'text'
+    value = value?value:''
+
+    let classNamestr = ['A187-input','input-medium-size','text']
+    classNamestr[1] = `input-${size}-size`
+    classNamestr[2] = type
+
+    const [inputValue,setInputValue] = useState<string>('')
+
+    if(value && value!==inputValue) setInputValue(value)
+
+    if(props.children) console.log(props.children)
+
+  return (
+    <div>
+        <input className={classNamestr.join(' ')} type={type} 
+        value={inputValue} placeholder={placeholder} onChange={(e)=>{
+            setInputValue(e.target.value)
+        }}/>
+    </div>
+        
+  )
+}
+
+export default Input
